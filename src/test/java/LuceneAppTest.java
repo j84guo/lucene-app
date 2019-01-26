6526efd28f64a2e51c0e6f0baa427e3bed038c4b
Jackson Guo <jacguo@jacguo-ld1.linkedin.biz>
2019-01-25 18:40:52 -0800

    update

--- src/test/java/LuceneAppTest.java
+++ src/test/java/LuceneAppTest.java
@@ -1,5 +1,7 @@
 import java.util.List;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.TermQuery;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -48,5 +50,16 @@ public class LuceneAppTest {
     List<Document> res = app.search("body", "bar");
     Assert.assertEquals(1, res.size());
     Assert.assertEquals("foo", res.get(0).get("title"));
+  }
+
+  // term query - field and value
+  @Test
+  public void testTermQuery() throws Exception {
+    LuceneApp app = new LuceneApp();
+    Term term = new Term("title", "go");
+    TermQuery query = new TermQuery(term);
+    List<Document> res = app.search(query);
+    Assert.assertEquals(1, res.size());
+    Assert.assertEquals("lang", res.get(0).getValues("body")[0]);
   }
 }
