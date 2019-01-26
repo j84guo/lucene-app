2ecfdda24f3aa49a6a75244165f26d679935be5d
Jackson Guo <jacguo@jacguo-ld1.linkedin.biz>
2019-01-25 18:26:06 -0800

    added example test

--- src/main/java/LuceneApp.java
+++ src/main/java/LuceneApp.java
@@ -19,108 +19,105 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
+
 public class LuceneApp {
 
-    private Directory index;
-    private Analyzer analyzer;
-    private IndexWriterConfig indexWriterConfig;
-    private IndexWriter indexWriter;
-    private IndexReader indexReader;
-    private IndexSearcher indexSearcher;
+  private Directory index;
+  private Analyzer analyzer;
+  private IndexWriterConfig indexWriterConfig;
+  private IndexWriter indexWriter;
+  private IndexReader indexReader;
+  private IndexSearcher indexSearcher;
 
-    private boolean shouldResetReader = false;
+  private boolean shouldResetReader = false;
 
-    private String[][] defaultDocuments = {
-        {"foo", "bar"},
-        {"go", "lang"},
-        {"alice", "bob"},
-        {"fire", "truck"}
-    };
+  private String[][] defaultDocuments = {{"foo", "bar"}, {"go", "lang"}, {"alice", "bob"}, {"fire", "truck"}};
 
-    public LuceneApp() throws IOException {
-        initIndex();
-        initWriter();
-        addDefaultDocuments();
+  public LuceneApp() throws IOException {
+    initIndex();
+    initWriter();
+    addDefaultDocuments();
+  }
+
+  private void initIndex() throws IOException {
+    index = new RAMDirectory();
+    analyzer = new StandardAnalyzer();
+  }
+
+  private void initWriter() throws IOException {
+    indexWriterConfig = new IndexWriterConfig(analyzer);
+    indexWriter = new IndexWriter(index, indexWriterConfig);
+  }
+
+  private void initReader() throws IOException {
+    indexReader = DirectoryReader.open(index);
+    indexSearcher = new IndexSearcher(indexReader);
+  }
+
+  private void resetReader() throws IOException {
+    closeReaderIfOpen();
+    initReader();
+  }
+
+  private void addDefaultDocuments() throws IOException {
+    for (String document[] : defaultDocuments) {
+      addDocument(document[0], document[1]);
+    }
+    commitWriter();
+  }
+
+  // assume all documents have title and body
+  public void addDocument(String title, String body) throws IOException {
+    Document doc = new Document();
+    doc.add(new TextField("title", title, Field.Store.YES));
+    doc.add(new TextField("body", body, Field.Store.YES));
+    indexWriter.addDocument(doc);
+  }
+
+  public void commitWriter() throws IOException {
+    indexWriter.commit();
+    closeReaderIfOpen();
+    shouldResetReader = true;
+  }
+
+  private void closeReaderIfOpen() throws IOException {
+    if (indexReader != null) {
+      indexReader.close();
+    }
+  }
+
+  // we provide a way to close both the reader and writer quietly
+  public void closeQuietly() {
+    try {
+      indexReader.close();
+    } catch (IOException e) {
     }
 
-    private void initIndex() throws IOException {
-        index = new RAMDirectory();
-        analyzer = new StandardAnalyzer();
+    try {
+      closeReaderIfOpen();
+    } catch (IOException e) {
+    }
+  }
+
+  public List<Document> search(String field, String queryText) throws ParseException, IOException {
+    if (shouldResetReader) {
+      resetReader();
+      shouldResetReader = false;
     }
 
-    private void initWriter() throws IOException {
-        indexWriterConfig = new IndexWriterConfig(analyzer);
-        indexWriter = new IndexWriter(index, indexWriterConfig);
+    Query query = new QueryParser(field, analyzer).parse(queryText);
+    TopDocs topDocs = indexSearcher.search(query, 10);
+
+    List<Document> results = new ArrayList<>();
+    for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+      results.add(indexSearcher.doc(scoreDoc.doc));
     }
 
-    private void initReader() throws IOException {
-        indexReader = DirectoryReader.open(index);
-        indexSearcher = new IndexSearcher(indexReader);
-    }
+    return results;
+  }
 
-    private void resetReader() throws IOException {
-        closeReaderIfOpen();
-        initReader();
-    }
-
-    private void addDefaultDocuments() throws IOException {
-        for (String document[] : defaultDocuments) {
-            addDocument(document[0], document[1]);
-        }
-        commitWriter();
-    }
-
-    // assume all documents have title and body
-    public void addDocument(String title, String body) throws IOException {
-        Document doc = new Document();
-        doc.add(new TextField("title", title, Field.Store.YES));
-        doc.add(new TextField("body", body, Field.Store.YES));
-        indexWriter.addDocument(doc);
-    }
-
-    public void commitWriter() throws IOException {
-        indexWriter.commit();
-        closeReaderIfOpen();
-        shouldResetReader = true;
-    }
-
-    private void closeReaderIfOpen() throws IOException {
-        if (indexReader != null) {
-            indexReader.close();
-        }
-    }
-
-    // we provide a way to close both the reader and writer quietly
-    public void closeQuietly() {
-        try {
-            indexReader.close();
-        } catch (IOException e) {}
-
-        try {
-            closeReaderIfOpen();
-        } catch (IOException e) {}
-    }
-
-    public List<Document> search(String field, String queryText) throws ParseException, IOException {
-        if (shouldResetReader) {
-            resetReader();
-            shouldResetReader = false;
-        }
-
-        Query query = new QueryParser(field, analyzer)
-            .parse(queryText);
-        TopDocs topDocs = indexSearcher.search(query, 10);
-
-        List<Document> results = new ArrayList<>();
-        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-            results.add(indexSearcher.doc(scoreDoc.doc));
-        }
-
-        return results;
-    }
-
-    public static void main(String[] args) throws Exception {
-        LuceneApp app = new LuceneApp();
-        System.out.println(app.search("body", "bar"));
-    }
+  public static void main(String[] args) throws Exception {
+    LuceneApp app = new LuceneApp();
+    System.out.println(app.search("body", "bar"));
+  }
 }
--- src/test/java/LuceneAppTest.java
+++ src/test/java/LuceneAppTest.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import org.apache.lucene.document.Document;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -5,6 +8,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
 
 public class LuceneAppTest {
   @BeforeSuite
@@ -37,8 +41,12 @@ public class LuceneAppTest {
     System.out.println("@AfterMethod");
   }
 
+  // assume default docs contain foo:bar
   @Test
-  public void test1() {
-    System.out.println("Test1 is running!");
+  public void testDefaultDocuments() throws Exception {
+    LuceneApp app = new LuceneApp();
+    List<Document> res = app.search("body", "bar");
+    Assert.assertEquals(1, res.size());
+    Assert.assertEquals("foo", res.get(0).get("title"));
   }
 }
